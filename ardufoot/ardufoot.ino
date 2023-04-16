fd47e6c59b0bf0ff65c7dc3a9c0c67ff3926a2e4
Rafael Bormann Chuede <rafaelchuede@gmail.com>
2023-04-16 14:45:06 -0300

    add segurar botao

--- ardufoot/ardufoot.ino
+++ ardufoot/ardufoot.ino
@@ -3,13 +3,17 @@
 
  
 
-const int nBotaoPush = 3;
-byte botaoPushPin[nBotaoPush] = {2,3,4};
+const int nBotaoPush = 4;
+byte botaoPushPin[nBotaoPush] = {2,3,4,5};
 byte botaoPushEstado[nBotaoPush] = {0};
 byte botaoPushEstadoP[nBotaoPush] = {0};
 byte outputValor[nBotaoPush] = {1};
 byte botaoPushVar[nBotaoPush] = {0};
-byte notaMidiPushBotao[nBotaoPush] = {64,65,66};
+byte seguradoVar[nBotaoPush] = {0};
+byte outputSegurado[nBotaoPush] = {1};
+unsigned long tempoSegurado[nBotaoPush] = {0};
+byte notaMidiPushBotao[nBotaoPush] = {64,65,66,67};
+byte pcSegurado[nBotaoPush] = {00,01,02,03};
 
 const int nBotaoToggle = 1;
 byte botaoTogglePin[nBotaoToggle] = {14};
@@ -18,8 +22,8 @@ byte botaoToggleEstadoP[nBotaoToggle] = {0};
 byte notaMidiToggleBotao[nBotaoToggle] = {70};
 
 
-const int nLeds = 3;
-byte ledPin[nLeds] = {6,7,8};
+const int nLeds = 4;
+byte ledPin[nLeds] = {6,7,8,9};
 
 byte channel = 5;
 
@@ -52,34 +56,60 @@ void loop() {
   // put your main code here, to run repeatedly:
 
  for(int i = 0; i < nBotaoPush; i++){
-   botaoPushEstado[i] = digitalRead(botaoPushPin[i]);
-   
-if((botaoPushEstado[i] == 0) && (botaoPushEstadoP[i] == 1)) {
- delay(50);
-  
-  outputValor[i]++;
-}if(outputValor[i]>2){
-  outputValor[i] = 1;
+  botaoPushEstado[i] = digitalRead(botaoPushPin[i]);
+
+  if((botaoPushEstado[i] == 0) && (botaoPushEstadoP[i] == 1)) {
+    tempoSegurado[i] = millis();
+if((botaoPushEstado[i] == 0) && (botaoPushEstadoP[i] == 1) && (millis() - tempoSegurado[i] < 25)){
+  delay(50);
+    outputValor[i]++;
+  }
+    // Botão pressionado
+    
+  } else if ((botaoPushEstado[i] == 1) && (botaoPushEstadoP[i] == 0)) {
+    // Botão solto
+    if (millis() - tempoSegurado[i] >= 1000) {
+      // Botão segurado por tempo suficiente
+      outputSegurado[i]++;
+      Serial.println("segurado");
+    } if(outputSegurado[i] > 2){
+      outputSegurado[i] = 1;
+    }
+  }
+
+  if(outputValor[i]>2){
+    outputValor[i] = 1;
+  }
+
+  botaoPushEstadoP[i] = botaoPushEstado[i];
+
+  if((outputValor[i] == 1) && (botaoPushVar[i] == 0)){
+    botaoPushVar[i]++; 
+    controlChange(channel, notaMidiPushBotao[i], 127);
+    MidiUSB.flush();
+    Serial.println("nota ON");
+    digitalWrite(ledPin[i], LOW);
+  }
+  if((outputValor[i] == 2) && (botaoPushVar[i] == 1)){
+    controlChange(channel, notaMidiPushBotao[i], 0);
+    MidiUSB.flush();
+    Serial.println("nota OFF");
+    botaoPushVar[i] = 0;
+    digitalWrite(ledPin[i], HIGH);
+  }
+if((outputSegurado[i] == 1) && (seguradoVar[i] == 0 )){
+  programChange(channel, pcSegurado[i]);
+  MidiUSB.flush();
+  seguradoVar[i]++;
+  } else if((outputSegurado[i] == 2) && (seguradoVar[i] == 1 )){
+     programChange(channel, pcSegurado[i]);
+  MidiUSB.flush();
+  seguradoVar[i] = 0;
+
+  }
+
 }
 
-botaoPushEstadoP[i] = botaoPushEstado[i];
-
-if((outputValor[i] == 1) && (botaoPushVar[i] == 0)){
-botaoPushVar[i]++; 
-controlChange(channel, notaMidiPushBotao[i], 127);
-MidiUSB.flush();
-Serial.println("nota ON");
-digitalWrite(ledPin[i], LOW);
-}
-if((outputValor[i] == 2) && (botaoPushVar[i] == 1)){
-  controlChange(channel, notaMidiPushBotao[i], 0);
-MidiUSB.flush();
-  Serial.println("nota OFF");
-  botaoPushVar[i] = 0;
-digitalWrite(ledPin[i], HIGH);
-}
-
- }
 // -------- Botao Toggle--------------------
  for(int i = 0; i < nBotaoToggle; i++){
    botaoToggleEstado[i] = digitalRead(botaoTogglePin[i]);
@@ -135,3 +165,7 @@ void controlChange(byte channel, byte control, byte value) {
   MidiUSB.sendMIDI(event);
 }
 
+void programChange(byte channel, byte program) {
+  midiEventPacket_t pc = {0x0C, 0xC0 | channel, program, 0};
+  MidiUSB.sendMIDI(pc);
+}
